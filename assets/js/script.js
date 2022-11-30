85cd03148a75b69549b88599d1100f20e23c1c35
Josiel <jsilverionascimento@gmail.com>
2022-11-30 17:22:38 -0300

    Project

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const products = [
+    { 
+        produto: 'Good Girl DOT Drama EDP',
+        peso: '80 ML',
+        precos: {
+            cartao: '10x R$ 49,99',
+            url: ,
+
+        }
+        }
+]
